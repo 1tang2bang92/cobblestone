a11be6118c6b0d3790ea021281709722662356b5
구잘 <dksgusals99@gmail.com>
2022-12-18 17:44:34 +0900

    Update main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { css, Global } from "@emotion/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+        }
+
+        #root {
+          width: 100%;
+          height: 100%;
+        }
+      `}
+    />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </React.StrictMode>
+);
